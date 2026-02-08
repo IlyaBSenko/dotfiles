f9bdda07a73caf59ae47f1af532d1998eed95340
Ilya Balatsenko <senkoet@gmail.com>
2026-02-08 00:21:42 -0500

    more configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.python3_host_prog = vim.fn.exepath("python")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,1 +1,32 @@
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "python",
+          "javascript",
+          "typescript",
+          "html",
+          "css",
+          "c",
+          "json",
+          "bash",
+          "ruby",
+          "haskell",
+          "rust",
+          "latex",
+          "markdown",
+        },
+        auto_install = true,
+      })
+    end,
+  },
+}
